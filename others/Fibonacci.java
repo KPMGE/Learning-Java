6a5a832db3806058d0cf64cbcaab8c2c3b6e85b5
KPMGE <jesuskevin254@gmail.com>
2021-11-15 13:32:56 -0300

    Adding fibonacci program

---
+++ others/Fibonacci.java
@@ -0,0 +1,32 @@
+// simple program to calculate the fibonacci serie.
+// Author: Kevin Carvalho de Jesus
+
+import java.util.Scanner;
+
+public class Fibonacci {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    int value = sc.nextInt();
+
+    System.out.println("Fibonacci sequence: ");
+    for (int i = 0; i < value; i++) {
+      System.out.print(fibonacci(i) + " ");
+    }
+
+    sc.close();
+  }
+
+  private static int fibonacci(int n) {
+    if (n == 0) {
+      return 0;
+    }
+
+    if (n == 1) {
+      return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
